00c8b3cc68730231c53e783bb7fd01c5ac9c0585
Karela King <karelaking277@gmail.com>
2025-10-27 17:12:30 +0530

    Enhance application setup by adding necessary modules and middleware for improved functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-
+import { ScheduleModule } from '@nestjs/schedule';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register(),
+    ScheduleModule.forRoot({}),
+    ThrottlerModule.forRoot({
+      throttlers: [{ ttl: 60, limit: 100 }],
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    EventEmitterModule.forRoot({ global: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
-import { ConsoleLogger } from '@nestjs/common';
+import session from 'express-session';
+import { ConsoleLogger, VersioningType } from '@nestjs/common';
 import { IncomingMessage, Server, ServerResponse } from 'http';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -13,7 +15,20 @@ async function bootstrap(): Promise<void> {
         timestamp: true,
         context: 'FinwiseApp',
       })
+    });
+  app.enableCors();
+  app.use(cookieParser());
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v1',
   });
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 
